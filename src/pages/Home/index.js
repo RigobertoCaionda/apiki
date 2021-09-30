7fb45343facc743b185d2c4b4617730e5f4e1658
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2021-09-30 09:40:25 +0100

    Creating the slideshow

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,63 @@
 import {
-		Container,
-		TitleBig, 
-		TitleSmall,
-		PageBanner,
-		ContainerSmall,
-		Title,
-		Text,
-		TextContainer,
-		TitleSm,
-		ImageArea,
-		Image
+		Container, TitleBig, 
+		TitleSmall, PageBanner,
+		ContainerSmall, Title,
+		Text, TextContainer,
+		TitleSm, ImageArea,
+		Image, SliderArea,
+		SliderTitle, OptArea,
+		SliderImage, ShareArea,
+		NextPreviousArea, FullSizeArea,
+		PreviousButton, NumberArea,
+		NextButton, ApikiCodeArea,
+		SliderWidth, Slider,
+		SliderImgContainer,	FormArea,
+		FormTitle1, FormTitle2
 	} from './styled';
+	import {Link} from 'react-router-dom';
+	import {useState, useRef, useEffect} from 'react';
 const Page = () => {
+	let TotalImages;//Para ser global
+	const [margin, setMargin] = useState(0);
+	const [currentPhoto, setCurrentPhoto] = useState(1);
+	let SliderImgContainerWidth = useRef();//Tamanho da imagem
+	const [totImg, setTotImg] = useState(0);//Gambiarra para retornar o numero de imagens que temos
+	useEffect(()=>{
+		TotalImages = document.querySelectorAll('.slider--width img').length;
+		setTotImg(TotalImages);
+	},[currentPhoto]);/*Se vc nao colocar dentro de um useEffect, vai retornar zero pois quando o codigo 
+		rodar ainda nenhuma imagem tera sido renderizada, so sera renderizada quando vc mudar algum item, 
+		entao eu coloquei ele a monitorar o currentPhoto, desse jeito, sempre que currentPhoto mudar, ele vai 
+		rerenderizar o let TotalImages (Sendo undefined) e depois vai entrar no useEffect e retornar o numero de 
+		imagens*/
+
+	let currentSlide = useRef(0);//Eu podia simplesmente colocar que currentSlide = 5, mas coloquei num useRef para que nao seja rerenderizado
+	const handleNextClick = () => {
+		currentSlide.current++;
+		setCurrentPhoto(currentPhoto + 1);//Para mudar o display que mostra que foto esta a ser exibida
+		if (currentSlide.current > (TotalImages - 1)) {
+			currentSlide.current = 0;
+			setCurrentPhoto(1);
+		}
+		updateMargin(currentSlide.current);//# entre useRef e useState, toda vez que vc usa um setState ha uma rerenderizacao
+	}
+
+	const handlePrevClick = () => {
+		currentSlide.current--;
+		setCurrentPhoto(currentPhoto - 1);
+		if (currentSlide.current < 0) {
+			currentSlide.current = (TotalImages - 1);
+			setCurrentPhoto(3);
+		}
+		updateMargin(currentSlide.current);
+	}
+
+	const updateMargin = (currentSlide) => {
+		let tam = SliderImgContainerWidth.current.clientWidth;
+		let newMargin = currentSlide * tam;
+		setMargin(newMargin);
+	}
+
 	return (
 			<Container>
 				<PageBanner>
@@ -79,7 +125,46 @@ const Page = () => {
 								transparência e não esperamos que outra pessoa assuma nossos compromissos.
 							</Text>
 					</TextContainer>
+					<SliderArea>
+						<SliderTitle>Conheça o Código de Cultura da Apiki</SliderTitle>
+
+						<Slider>
+							<SliderWidth margin={margin} total={totImg} className="slider--width">
+								<SliderImgContainer ref={SliderImgContainerWidth}>
+									<SliderImage src="/assets/cocacola1.png" />
+								</SliderImgContainer>
+
+								<SliderImgContainer>
+									<SliderImage src="/assets/mocaLinda.jpg" />
+								</SliderImgContainer>
+
+								<SliderImgContainer>
+									<SliderImage src="/assets/tubidy2.png" />
+								</SliderImgContainer>
+
+							</SliderWidth>
+
+							<OptArea>
+								<ShareArea></ShareArea>
+								<NextPreviousArea>
+									<PreviousButton onClick={handlePrevClick}>p</PreviousButton>
+									<NumberArea>{currentPhoto} of {totImg}</NumberArea>
+									<NextButton onClick={handleNextClick}>n</NextButton>
+								</NextPreviousArea>
+								<FullSizeArea></FullSizeArea>
+							</OptArea>
+							<ApikiCodeArea>
+								<Link to="/apiki_cultura">Código de cultura da Apiki </Link>
+									 from <Link to="/apiki_wordpress"> Apiki Wordpress</Link>
+							</ApikiCodeArea>
+						</Slider>
+					</SliderArea>
 				</ContainerSmall>
+
+				<FormArea>
+					<FormTitle1>Oportunidades de Carreira na Apiki</FormTitle1>
+					<FormTitle2>Chega mais, vem ser um Apiker!</FormTitle2>
+				</FormArea>
 			</Container>
 		);
 }
--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -43,3 +43,73 @@ export const ImageArea = styled.div`
 export const Image = styled.img`
 	width: 100%;
 `;
+export const SliderArea = styled.div`
+	overflow: hidden;
+`;
+export const SliderTitle = styled.h1``;
+export const OptArea = styled.div`
+	height: 50px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 0 30px;
+	background-color: #e9f7fd;
+	border: 1px solid #ccc;
+	margin-bottom: 20px;
+`;
+export const SliderImage = styled.img`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+`;
+export const ShareArea = styled.div`
+	width: 15px;
+	height: 15px;
+	background-color: #000;
+`;
+export const NextPreviousArea = styled.div`
+	width: 150px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+`;
+export const FullSizeArea = styled.div`
+	width: 15px;
+	height: 15px;
+	background-color: #000;
+`;
+export const NextButton = styled.div``;
+export const NumberArea = styled.div``;
+export const PreviousButton = styled.div``;
+export const ApikiCodeArea = styled.div`
+	a {
+		text-decoration: none;
+		font-weight: bold;
+		color: #e36000;
+	}
+`;
+export const SliderWidth = styled.div`
+	width: calc(770px * ${props=>props.total});
+	display: flex;
+	transition: all ease 0.3s;
+	min-height: 300px;
+	margin-left: -${props=>props.margin}px;
+`;
+export const Slider = styled.div``;
+export const SliderImgContainer = styled.div`
+	width: 100%;
+	height: 350px;
+`;
+export const FormArea = styled.div`
+	width: 1200px;
+	margin: 180px auto 50px auto;
+	background-color: #f1f1f1;
+	border-radius: 20px;
+	padding: 100px 0 80px 0;
+`;
+export const FormTitle1 = styled.h1`
+	text-align: center;
+`;
+export const FormTitle2 = styled.h2`
+	text-align: center;
+`;
